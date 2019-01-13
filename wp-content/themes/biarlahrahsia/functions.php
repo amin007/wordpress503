0e12a0a73a4d1a4fd81f58a01f6f4dea9bbca3f2
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-01-13 17:08:48 +0800

    tambah latihan03

--- wp-content/themes/biarlahrahsia/functions.php
+++ wp-content/themes/biarlahrahsia/functions.php
@@ -1,10 +1,20 @@
 <?php
-/*
+
 function biarlah_script_enqueue()
 {
-	wp_enqueue_style('customstyle', get_template_directory_url() . '/css/biarlah.css', array(), '1.0.0', 'all');
-	wp_enqueue_script('customjs', get_template_directory_url() . '/js/biarlah.js', array(), '1.0.0', true);# true -> letak js di bawah
+	wp_enqueue_style('customstyle', get_template_directory_uri() . '/css/biarlah.css', array(), '1.0.0', 'all');
+	wp_enqueue_script('customjs', get_template_directory_uri() . '/js/biarlah.js', array(), '1.0.0', true);# true -> letak js di bawah
 }
 
 add_action('wp_enqueue_scripts','biarlah_script_enqueue');
+/*
+function biarlah_theme_setup()
+{
+	add_theme_suport('menus');
+
+	register_nav_menu('primary','Primary Header Navigarion');
+	register_nav_menu('secondary','Footer Navigation');
+}
+
+add_action('init','biarlah_theme_setup');
 //*/
