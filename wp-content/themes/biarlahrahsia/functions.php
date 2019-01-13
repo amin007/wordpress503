f87ad7076e2327752e36ffd8b2d8e86266049aec
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-01-13 15:56:06 +0800

    tambah latihan02

---
+++ wp-content/themes/biarlahrahsia/functions.php
@@ -0,0 +1,10 @@
+<?php
+/*
+function biarlah_script_enqueue()
+{
+	wp_enqueue_style('customstyle', get_template_directory_url() . '/css/biarlah.css', array(), '1.0.0', 'all');
+	wp_enqueue_script('customjs', get_template_directory_url() . '/js/biarlah.js', array(), '1.0.0', true);# true -> letak js di bawah
+}
+
+add_action('wp_enqueue_scripts','biarlah_script_enqueue');
+//*/
