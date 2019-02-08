04901ad93aa97ef5738ba8d596a7d8fd8913594e
Amin Ledang @ Dell <helmi007@yahoo.com>
2019-02-08 12:55:34 +0800

    tambah fail baru untuk plugin wordpress

---
+++ wp-content/plugins/rahsia-plugin/rahsia-plugin.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * @package RahsiaPlugin
+ */
+/*
+Plugin Name: Rahsia Plugin
+Plugin URI: http://amin007.github.io/plugin
+Desciption : Ini percubaan pertama untuk menulis plugin dalam wordpress daa
+Version: 1.0.0
+Author: Amin Ledang
+Authir URI: http://amin007.github.io/siapasaya
+License: GPLv2 or later
+Text Domain: rahsia-plugin
+*/
